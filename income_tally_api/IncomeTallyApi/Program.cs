2ad7f44e4b1ad74a5e737b794fd5bf4665514b53
MartinPHt <marty.papalakov@gmail.com>
2024-11-26 23:20:39 +0200

    IncomeTally API: Implemented functionality for Registering CORS services in DI container to resolve issue when running flutter UI app in chrome.

--- income_tally_api/IncomeTallyApi/Program.cs
+++ income_tally_api/IncomeTallyApi/Program.cs
@@ -2,14 +2,32 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Register CORS services in DI container
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin()  // Allow requests from any origin
+              .AllowAnyMethod()  // Allow any HTTP method (GET, POST, etc.)
+              .AllowAnyHeader(); // Allow any headers
+    });
+});
+
+// Add MVC services to the container
+builder.Services.AddControllersWithViews();
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Enable CORS globally (for all routes)
+app.UseCors("AllowAllOrigins");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
